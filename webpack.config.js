83bda18d71ad96f3c39b2025b556b345fc37a3e1
Claire Annan <claire.annan@gsa.gov>
2020-11-13 19:36:14 -0500

    First commit on veen theme.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/index.js",
+  devServer: {
+    contentBase: "./dist",
+  },
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env"]],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass"),
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
